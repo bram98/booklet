e686973ee97558e6fa47e7d6e46dc2e83984bab1
Bram Verreussel <b.verreussel@gmail.com>
2023-12-22 11:11:11 +0100

    added process_proj_Descriptions, not very complete

---
+++ process_proj_descriptions.py
@@ -0,0 +1,81 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from pathlib import Path
+from glob import glob
+from docx import Document
+from docx.shared import Inches
+import doc2docx
+from tqdm import tqdm
+import re
+
+#%%
+
+os.chdir(os.path.dirname(__file__))
+
+#%%
+def convert_doc_to_docx_windows():
+    print('Converting .doc files to .docx files...')
+    doc_files = glob('response_data/project_descriptions_renamed/*.doc')
+    for doc_file in tqdm(doc_files):
+        doc2docx.convert(doc_file)
+    print('\nDone converting .doc files to .docx files')
+    
+    print('Removing old .doc files...')
+    for doc_file in tqdm(doc_files):
+        Path(doc_file).unlink( missing_ok=True)
+    
+    print('\nDone removing old .doc files...')
+
+def inspect_file_types():
+    filenames = glob('response_data/project_descriptions_renamed/*')
+    for filename in filenames:
+        ext  = Path(filename).suffix
+        if ext != '.docx':
+            print(Path(filename).name)
+
+# convert_doc_to_docx_windows()
+# inspect_file_types()
+
+#%%
+filenames = glob('response_data/project_descriptions_renamed/*.docx')
+
+reference_regex = re.compile(r'(^\[\d\])|references:?', flags=re.M|re.I)
+info_regex = re.compile(r'^(sponsors?)|(supervisors?)|(keywords)', flags=re.M|re.I)
+figure_regex = re.compile(r'^fig(ure)?', flags=re.I)
+email_regex = re.compile(r'@', flags=re.I)
+# lengths = []
+for filename in filenames[:40]:
+    doc = Document(filename)
+    # print( [len(p.text) for p in doc.paragraphs] )
+    paras = [p.text for p in doc.paragraphs]
+    for para in paras:
+        if len(para) == 0:
+            continue
+        if not reference_regex.search(para) is None:
+            # print(para)
+            # print(len(para))
+            break
+        if not info_regex.search(para) is None:
+            break
+        
+        if not figure_regex.search(para) is None:
+            # print(para)
+            break
+        if not email_regex.search(para) is None:
+            print(para)
+            # break
+        
+        if len(para) < 250:
+            # print(para)
+            print('')
+    # lengths.append([len(p.text) for p in doc.paragraphs])
+    
+
+# lengths_flattened = [length for row in lengths for length in row]
+
+#%%
+plt.figure()
+plt.ylim(0,50)
+plt.hist(lengths_flattened, bins=200)
