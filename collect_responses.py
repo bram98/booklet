5ab7a989b6e3baf0f29be04bdc29ae3a488462a6
A.H. Kole <a.h.kole@uu.nl>
2024-01-16 12:55:22 +0100

    Modified collect_responses to skip over incorrect submissions + added ref docx with correct font (size)

--- collect_responses.py
+++ collect_responses.py
@@ -18,7 +18,11 @@ for i, row in df.iterrows():
     makedirs(dest_dir, exist_ok=True)
 
     # Copy the image files
-    file_src = glob(f'response_data/portraits_renamed/portrait {row["ID"]} {row["Full Name"]}.*')[0]
+    file_src = glob(f'response_data/portraits_renamed/portrait {row["ID"]} {row["Full Name"]}.*')
+    if len(file_src) > 0:
+        file_src = file_src[0]
+    else:
+        continue  # Skip these students for now
     file_dest = osp.join(dest_dir, osp.basename(file_src))
     copy(file_src, file_dest)
 
@@ -30,15 +34,23 @@ for i, row in df.iterrows():
 
     # Create the word file with project description and other info
     md_string = f"""\
-| {row['Project Title']}
+| **{row['Project Title']}**
 
-| {row['Full Name']} ({row['Project Type']}), {row['E-mail']}
+| **{row['Full Name']}** ({row['Project Type']}), {row['E-mail']}
+
 | **Sponsor:** {row['Sponsor(s)']}
 | **Supervisor(s):** {row['Supervisor(s)']}
+
 | **Keyword(s):** {row['Keywords']}
 
 """
-    file_src = glob(f'response_data/project_descriptions_renamed/project_description {row["ID"]} {row["Full Name"]}.*')[0]
+    file_src = glob(f'response_data/project_descriptions_renamed/project_description {row["ID"]} {row["Full Name"]}.*')
+    if len(file_src) > 0:
+        file_src = file_src[0]
+    else:
+        continue  # Skip these students for now
+    if osp.splitext(file_src)[1] != '.docx':
+        continue  # Skip these students for now
     project_description = pandoc.read(file=file_src)
     md_string += pandoc.write(project_description)
     fig_caption = row['Figure Caption (optional)']
@@ -51,8 +63,27 @@ Figure 1: {fig_caption.strip()}
 """
 
     # Here we have to add the references (bibliography) to the md_string
+    file_src = glob(f'response_data/references_renamed/references {row["ID"]} {row["Full Name"]}.*')
+    if len(file_src) > 0:  # References are optional
+        if osp.splitext(file_src[0])[1] == '.docx':  # Skip students with bib files for now
+            continue
+        try:
+            md_string += '\n'  # Add an empty line to separate from previous part
+            ref_df = pd.read_excel(file_src[0]).sort_values(by='Number')
+            for i, ref_row in ref_df.iterrows():
+                if is_any_real_numeric_dtype(type(ref_row["Issue"])) and np.isnan(ref_row["Issue"]):
+                    md_string += f'[{ref_row["Number"]}] {ref_row["First author"]} *et al.*, *{ref_row["Journal"]}* ({ref_row["year"]})  \n'
+                else:
+                    md_string += f'[{ref_row["Number"]}] {ref_row["First author"]} *et al.*, *{ref_row["Journal"]}* **{ref_row["Issue"]}**: {ref_row["page range"]} ({ref_row["year"]})  \n'
+        except Exception:
+            continue  # Skip these students for now
+    else:
+        # Add check here whether there are citations in the md_string so far, which would mean that there should be a ref file
+        pass
 
+
+        
     # Write the md_string to a Word file
     doc = pandoc.read(md_string)
     file_dest = osp.join(dest_dir, f'project_description {row["ID"]} {row["Full Name"]}.docx')
-    pandoc.write(doc, file=file_dest, format='docx')
+    pandoc.write(doc, file=file_dest, format='docx', options=['--reference-doc=custom-reference.docx'])
